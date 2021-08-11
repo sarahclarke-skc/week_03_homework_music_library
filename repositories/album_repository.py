4dbf73e2c2e0159f360cdb10daf4fd8854501765
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-11 09:52:15 +0100

    changed artist_id to artist.id following review

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -48,7 +48,7 @@ def select_all():
 
 def update(album):
     sql = "UPDATE albums SET (title, genre, artist_id) = (%s, %s, %s) WHERE id = %s"
-    values = [album.title, album.genre, album.artist_id, album.id]
+    values = [album.title, album.genre, album.artist.id, album.id]
     run_sql(sql, values)
 
 # (Pdb) album_repository.all_albums_by_artist("Michael Jackson")
